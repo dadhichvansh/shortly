763cfe6c5b03000ebcd9461af1f9dfad1720b3d5
vansh-home <dadhichvansh46@gmail.com>
2025-07-26 01:02:20 +0530

    update drizzle.config.js file

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-  out: "./drizzle/migrations",
+  out: "./drizzle",
   schema: "./db/schema.js",
   dialect: "mysql",
   dbCredentials: {
