09b3b254b2d8aa77670c877a8a9f286cb6d8da62
vansh-home <dadhichvansh46@gmail.com>
2025-07-27 20:13:46 +0530

    update import statements

--- main.js
+++ main.js
@@ -1,15 +1,19 @@
 import express from "express";
+import path from "path";
 import { PORT } from "./validation.js";
-import { shortenerRoute } from "./routes/shortener.route.js";
+import { shortenerRoute } from "./src/routes/shortener.route.js";
 
 const app = express();
+const viewsPath = path.join(import.meta.dirname, "src", "views");
 
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
+app.set("views", viewsPath);
+
 app.use(shortenerRoute);
 
 app.listen(PORT, () => {
-  console.log(`Listening on http://localhost:${PORT}`);
+  console.log(`Listening to server on http://localhost:${PORT}/`);
 });
