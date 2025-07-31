dec951f04e4ffa3a8837d4ea6319575e5d85dabb
vansh-home <dadhichvansh46@gmail.com>
2025-07-31 23:00:29 +0530

    update src/controllers/shortener.controller.js

--- src/controllers/shortener.controller.js
+++ src/controllers/shortener.controller.js
@@ -3,32 +3,49 @@ import {
   fetchShortenedUrls,
   fetchShortenedUrlByShortCode,
   saveShortenedUrl,
+  findShortLinkById,
+  updateShortCode,
+  deleteShortenedUrlById,
 } from "../services/shortener.service.js";
+import { z } from "zod";
 
-const fetchShortenedUrl = async (req, res) => {
+export const fetchShortenedUrl = async (req, res) => {
   try {
-    const links = await fetchShortenedUrls();
-    res.render("index", { links, host: req.host });
+    if (!req.user) return res.redirect("/login");
+
+    const links = await fetchShortenedUrls(req.user.id);
+
+    res.render("index", { links, host: req.host, errors: req.flash("errors") });
   } catch (error) {
     console.error(error);
     res.status(500).send("Internal server error.");
   }
 };
 
-const createShortenedUrl = async (req, res) => {
+export const createShortenedUrl = async (req, res) => {
   try {
-    const { url, shortCode } = req.body;
+    if (!req.user) return res.redirect("/login");
 
+    const { url, shortCode } = req.body;
     const finalShortCode = shortCode || crypto.randomBytes(4).toString("hex");
     const link = await fetchShortenedUrlByShortCode(shortCode);
 
     if (link) {
-      return res
-        .status(400)
-        .send("Short code already exists. Try choosing another.");
+      // return res
+      //   .status(400)
+      //   .send("Short code already exists. Try choosing another.");
+      req.flash(
+        "errors",
+        "URL with that short code already exists, please choose another."
+      );
+      return res.redirect("/");
     }
 
-    await saveShortenedUrl({ url, finalShortCode });
+    await saveShortenedUrl({
+      url,
+      shortCode: finalShortCode,
+      userId: req.user.id,
+    });
 
     res.redirect("/");
   } catch (error) {
@@ -37,7 +54,7 @@ const createShortenedUrl = async (req, res) => {
   }
 };
 
-const redirectToShortenedUrl = async (req, res) => {
+export const redirectToShortenedUrl = async (req, res) => {
   try {
     const { shortCode } = req.params;
     const link = await fetchShortenedUrlByShortCode(shortCode);
@@ -50,4 +67,74 @@ const redirectToShortenedUrl = async (req, res) => {
   }
 };
 
-export { fetchShortenedUrl, createShortenedUrl, redirectToShortenedUrl };
+export const fetchShortenedUrlEditPage = async (req, res) => {
+  try {
+    if (!req.user) return res.redirect("/login");
+
+    const { data: id, error } = z.coerce
+      .number()
+      .int()
+      .safeParse(req.params.id);
+
+    if (error) return res.redirect("/404");
+
+    const shortLink = await findShortLinkById(id);
+    if (!shortLink) return res.redirect("/404");
+
+    res.render("edit-shortLink", {
+      id: shortLink.id,
+      url: shortLink.url,
+      shortCode: shortLink.shortCode,
+      errors: req.flash("errors"),
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error.");
+  }
+};
+
+export const editShortenedUrl = async (req, res) => {
+  try {
+    if (!req.user) return res.redirect("/login");
+
+    const { data: id, error } = z.coerce
+      .number()
+      .int()
+      .safeParse(req.params.id);
+
+    if (error) return res.redirect("/404");
+
+    const { url, shortCode } = req.body;
+    const newUpdatedShortCode = await updateShortCode({ id, url, shortCode });
+
+    if (!newUpdatedShortCode) return res.redirect("/404");
+    res.redirect("/");
+  } catch (error) {
+    if (error.code === "ER_DUP_ENTRY") {
+      req.flash("errors", "Shortcode already exists, please choose another.");
+      return res.redirect(`/edit/${id}`);
+    }
+
+    console.error(error.message);
+    return res.status(500).send("Internal server error.");
+  }
+};
+
+export const deleteShortenedUrl = async (req, res) => {
+  try {
+    if (!req.user) return res.redirect("/login");
+
+    const { data: id, error } = z.coerce
+      .number()
+      .int()
+      .safeParse(req.params.id);
+
+    if (error) return res.redirect("/404");
+
+    await deleteShortenedUrlById(id);
+    res.redirect("/");
+  } catch (error) {
+    console.error(error.message);
+    return res.status(500).send("Internal server error.");
+  }
+};
