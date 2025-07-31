43974f2708e898e308eb7b3cdc41bb08e1e1825e
vansh-home <dadhichvansh46@gmail.com>
2025-07-31 23:05:54 +0530

    update src/services/shortener.service.js

--- src/services/shortener.service.js
+++ src/services/shortener.service.js
@@ -2,8 +2,11 @@ import { eq } from "drizzle-orm";
 import { db } from "../db/db.js";
 import { shortLinksTable } from "../drizzle/schema.js";
 
-export const fetchShortenedUrls = async () => {
-  return await db.select().from(shortLinksTable);
+export const fetchShortenedUrls = async (userId) => {
+  return await db
+    .select()
+    .from(shortLinksTable)
+    .where(eq(shortLinksTable.userId, userId));
 };
 
 export const fetchShortenedUrlByShortCode = async (shortCode) => {
@@ -14,6 +17,25 @@ export const fetchShortenedUrlByShortCode = async (shortCode) => {
   return result;
 };
 
-export const saveShortenedUrl = async ({ url, finalShortCode }) => {
-  await db.insert(shortLinksTable).values({ url, shortCode: finalShortCode });
+export const saveShortenedUrl = async ({ url, shortCode, userId }) => {
+  await db.insert(shortLinksTable).values({ url, shortCode, userId });
+};
+
+export const findShortLinkById = async (id) => {
+  const [result] = await db
+    .select()
+    .from(shortLinksTable)
+    .where(eq(shortLinksTable.id, id));
+  return result;
+};
+
+export const updateShortCode = async ({ id, url, shortCode }) => {
+  return await db
+    .update(shortLinksTable)
+    .set({ url, shortCode })
+    .where(eq(shortLinksTable.id, id));
+};
+
+export const deleteShortenedUrlById = async (id) => {
+  return await db.delete(shortLinksTable).where(eq(shortLinksTable.id, id));
 };
