c05f5d3532bc2760622ea80669b26d512383ef95
vansh-home <dadhichvansh46@gmail.com>
2025-09-08 21:13:27 +0530

    update and add auth routes

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,18 +1,37 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   registerPage,
   userRegister,
   loginPage,
   userLogin,
-  fetchProfile,
   userLogout,
-} from "../controllers/auth.controller.js";
+  fetchProfilePage,
+  fetchEditProfilePage,
+  updateUserProfile,
+  fetchVerifyEmailPage,
+  resendVerificationLink,
+  verifyEmailToken,
+} from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.route("/register").get(registerPage).post(userRegister);
-router.route("/login").get(loginPage).post(userLogin);
-router.route("/profile").get(fetchProfile);
-router.route("/logout").get(userLogout);
+// User registration and login routes
+router.get('/register', registerPage);
+router.post('/register', userRegister);
+
+router.get('/login', loginPage);
+router.post('/login', userLogin);
+
+// Profile and logout routes
+router.get('/profile', fetchProfilePage);
+router.get('/edit-profile', fetchEditProfilePage);
+router.post('/edit-profile', updateUserProfile);
+
+router.post('/logout', userLogout);
+
+// Email verification routes
+router.get('/verify-email', fetchVerifyEmailPage);
+router.post('/resend-verification-link', resendVerificationLink);
+router.get('/verify-email-token', verifyEmailToken);
 
 export const authRoutes = router;
