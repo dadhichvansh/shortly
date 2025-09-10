990b29ccc76b006c8cbf0e75d86b983f783bbd50
vansh-home <dadhichvansh46@gmail.com>
2025-09-11 01:37:59 +0530

    add routes for deleting and editing shortened urls

--- src/routes/shortener.route.js
+++ src/routes/shortener.route.js
@@ -1,14 +1,23 @@
-import { Router } from "express";
+import { Router } from 'express';
 import {
   fetchShortenedUrl,
   createShortenedUrl,
   redirectToShortenedUrl,
-} from "../controllers/shortener.controller.js";
+  fetchShortenedUrlEditPage,
+  editShortenedUrl,
+  deleteShortenedUrl,
+} from '../controllers/shortener.controller.js';
 
 const router = Router();
 
-router.get("/", fetchShortenedUrl);
-router.post("/", createShortenedUrl);
-router.get("/:shortCode", redirectToShortenedUrl);
+// Fetch the main page, create a new shortened URL, and redirect to the original URL
+router.get('/', fetchShortenedUrl);
+router.post('/', createShortenedUrl);
+router.get('/:shortCode', redirectToShortenedUrl);
+
+// Additional routes for editing and deleting shortened URLs
+router.get('/edit/:id', fetchShortenedUrlEditPage);
+router.post('/edit/:id', editShortenedUrl);
+router.post('/delete/:id', deleteShortenedUrl);
 
 export const shortenerRoute = router;
