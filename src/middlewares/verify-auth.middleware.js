0fe5ebfa647f391216ef90a73d4c3f75525c226e
vansh-home <dadhichvansh46@gmail.com>
2025-09-10 22:59:56 +0530

    update auth verification middleware

--- src/middlewares/verify-auth.middleware.js
+++ src/middlewares/verify-auth.middleware.js
@@ -1,20 +1,63 @@
-import { verifyJwt } from "../services/auth.service.js";
+import {
+  ACCESS_TOKEN_EXPIRY,
+  MILLISECONDS_PER_SECOND,
+  REFRESH_TOKEN_EXPIRY,
+} from '../constants.js';
+import { refreshTokens, verifyJwt } from '../services/auth.service.js';
 
-export const verifyAuthentication = (req, res, next) => {
-  const token = req.cookies.access_token;
+export const verifyAuthentication = async (req, res, next) => {
+  const accessToken = req.cookies.access_token;
+  const refreshToken = req.cookies.refresh_token;
 
-  if (!token) {
+  if (!accessToken && !refreshToken) {
     req.user = null;
     return next();
   }
 
-  try {
-    const decodedToken = verifyJwt(token);
-    req.user = decodedToken;
-  } catch (error) {
-    req.user = null;
-    console.error(error.message);
+  if (accessToken) {
+    try {
+      const decodedToken = verifyJwt(accessToken);
+      req.user = decodedToken;
+      return next();
+    } catch (error) {
+      console.error(
+        'Error populating access_token to req.user:',
+        error.message
+      );
+    }
   }
 
+  if (refreshToken) {
+    try {
+      const { newAccessToken, newRefreshToken, user } = await refreshTokens(
+        refreshToken
+      );
+
+      req.user = user;
+      const baseConfig = { httpOnly: true, secure: true };
+
+      res.cookie('access_token', newAccessToken, {
+        ...baseConfig,
+        maxAge: ACCESS_TOKEN_EXPIRY / MILLISECONDS_PER_SECOND,
+      });
+      res.cookie('refresh_token', newRefreshToken, {
+        ...baseConfig,
+        maxAge: REFRESH_TOKEN_EXPIRY / MILLISECONDS_PER_SECOND,
+      });
+
+      return next();
+    } catch (error) {
+      console.error('An error occurred while refreshing tokens:', error);
+
+      // Clear cookies and set user as null upon refresh failure
+      res.clearCookie('access_token');
+      res.clearCookie('refresh_token');
+      req.user = null;
+
+      return next();
+    }
+  }
+
+  req.user = null;
   return next();
 };
